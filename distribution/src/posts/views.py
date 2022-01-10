f412fc5cc3146c3975a6666eae898a4134f4657a
Mola <zengxuankai034@gmail.com>
2022-01-10 13:49:04 +0800

    Function Addition -’POST‘ 2nd & -’Admin‘ 2nd & -'marketing' 1st

--- distribution/src/posts/views.py
+++ distribution/src/posts/views.py
@@ -1,10 +1,23 @@
+from django.db.models.fields import EmailField
 from django.shortcuts import render
 from .models import Post
+from marketing.models import Signup
 
 def index(request):
-    queryset = Post.objects.filter(featured = True)
+    # 展现featured状态博客
+    featured = Post.objects.filter(featured = True)
+    # 最近三篇博客
+    latest = Post.objects.order_by('-timestamp')[0:3]
+
+    if request.method == "POST":
+        email = request.POST["email"]
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+        
     context = {
-        'object_list' : queryset
+        'object_list' : featured,
+        'latest' : latest,
     }
     return render(request, 'index.html', context)   
 
